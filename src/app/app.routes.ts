67188f19931e4e0b2c9f0b327369680f5b232036
Chuyini <jmlr231201@gmail.com>
2025-04-12 11:18:24 -0600

    change node

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 
 import { HomePage } from './pages/home/home.page';
+import { GratitudeComponent } from './pages/gratitude/gratitude.page';
 
-export const routes: Routes = [{
-    path: "", component: HomePage
-}];
+export const routes: Routes = [
+    { path: "", component: HomePage },
+    { path: "gratitude", component: GratitudeComponent }
+];
